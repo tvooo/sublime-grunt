911ad9f45c8fc575c4fc0c3b1f97004c9cfd07fd
Tim von Oldenburg <tim@voldenburg.com>
2012-10-12 10:48:44 +0200

    Added standard tasks (lint etc.)

--- main.py
+++ main.py
@@ -1,8 +1,21 @@
-import sublime, sublime_plugin
-from xml.etree import ElementTree as ET
+import sublime
+import sublime_plugin
 import os
 import re
-import subprocess
+import sys
+#import subprocess
+
+basicTasks = [
+    ["lint", "Validate files with JSHint."],
+    ["qunit", "Run QUnit unit tests in a headless PhantomJS instance."],
+    ["min", "Minify files with UglifyJS."],
+    ["watch", "Run predefined tasks whenever watched files change."],
+    ["server", "Start a static web server."],
+    ["concat", "Concatenate files."],
+    ["test", "Run unit tests with nodeunit."],
+    ["init", "Generate project scaffolding from a predefined template."]
+]
+
 
 class GruntConsole(object):
     def __init__(self, window):
@@ -14,6 +27,7 @@ class GruntConsole(object):
         v.set_name("Grunt")
         v.set_scratch(True)
         return v
+
 
 class GruntfileParser(object):
     def __init__(self, window):
@@ -31,21 +45,21 @@ class GruntfileParser(object):
                 self.window.show_quick_panel(self.grunt_files, self.choose_file)
         else:
             sublime.error_message("Gruntfile not found!")
-        
+
     def choose_file(self, file):
         self.tasks = []
         try:
             f = open(self.grunt_files[0], "r")
             #s = f.read()
-            regex = re.compile( r'registerTask\(\'(.*)\', \'(.*)\'\)', re.M|re.I)
+            regex = re.compile(r'registerTask\(\'(.*)\', \'(.*)\'\)', re.M | re.I)
             for line in f:
                 match = regex.search(line)
                 if match:
                     self.tasks.append(list(match.groups()))
-                #else:
-                #    print "No match!!"
+                    print list(match.groups())
+            self.tasks = self.tasks + basicTasks
         except IOError:
-            sys.stderr.write( "[myScript] - Error: Could not open %s\n" % (inputFn) )
+            sys.stderr.write("[myScript] - Error: Could not open %s\n" % (self.grunt_files[0]))
             sys.exit(-1)
 
         self.window.show_quick_panel(self.tasks, self.on_done)
@@ -55,10 +69,12 @@ class GruntfileParser(object):
         exec_args.update({'cmd': u"grunt --no-color " + self.tasks[task][0], 'shell': True, 'working_dir': self.folders[0]})
         self.window.run_command("exec", exec_args)
 
+
 def settings():
     return sublime.load_settings('SublimeGrunt.sublime-settings')
+
 
 class GruntCommand(sublime_plugin.TextCommand):
     def run(self, edit):
         self.window = self.view.window()
-        g = GruntfileParser(self.window)
+        GruntfileParser(self.window)
