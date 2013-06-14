e85370e9c7a45a5c12ccdba6c574f6992b5326f3
Tim von Oldenburg <tim@tvooo.de>
2013-06-14 17:49:31 +0200

    Added a command to kill tasks

--- README.md
+++ README.md
@@ -8,10 +8,12 @@ A Grunt task runner for Sublime Text
 Open the command palette using Ctrl+Shift+P (or Cmd+Shift+P on Mac, respectively)
 and choose the "Grunt" command.
 
-The plugin expects to find a Grunt file (`grunt.js` or `Gruntfile.js`) in an open folder.
-It displays a list of available Grunt tasks out of this grunt file.
+The plugin expects to find a Grunt file (`Gruntfile.js`) in an open folder.
+It displays a list of available Grunt tasks out of this Grunt file.
 If it finds more than one, it first provides a list for selection.
 
+As of version 0.2, there is also a command to kill running tasks, for example
+`watch` tasks.
 
 ## Settings
 
@@ -19,9 +21,14 @@ The file `SublimeGrunt.sublime-settings` is used for configuration.
 You may have to add the path to your Node.js installation to the `path`
 variable.
 
+## Releases
+
+* 0.1 Initial release
+* 0.2 Rewrite; supports Grunt >= 0.4, tasks can be killed (for example the `watch` task)
+
 ## Thanks
 
-Thanks for some contributions to
+Thanks for some contributions go to
 
 * @antonellopasella
 * @structAnkit
--- main.py
+++ main.py
@@ -6,30 +6,7 @@ import sys
 import subprocess
 import json
 
-legacyBuiltinTasks = [
-    ["lint", "Validate files with JSHint."],
-    ["qunit", "Run QUnit unit tests in a headless PhantomJS instance."],
-    ["min", "Minify files with UglifyJS."],
-    ["watch", "Run predefined tasks whenever watched files change."],
-    ["server", "Start a static web server."],
-    ["concat", "Concatenate files."],
-    ["test", "Run unit tests with nodeunit."],
-    ["init", "Generate project scaffolding from a predefined template."]
-]
-
 tasksJSON = re.compile(r'EXPOSE_BEGIN(.*)EXPOSE_END', re.M | re.I | re.DOTALL)
-
-class GruntConsole(object):
-    def __init__(self, window):
-        self.window = window
-
-    @property
-    def view(self):
-        v = self.window.new_file()
-        v.set_name("Grunt")
-        v.set_scratch(True)
-        return v
-
 
 class GruntRunner(object):
     def __init__(self, window):
@@ -41,7 +18,7 @@ class GruntRunner(object):
         path = settings().get('exec_args').get('path')
         package_path = os.path.join(sublime.packages_path(), "sublime-grunt")
         args = 'grunt --no-color --tasks "' + package_path + '" expose'
-        
+
         p = subprocess.Popen( args, stdout=subprocess.PIPE, env={"PATH": "/usr/local/bin"}, cwd=self.wd, shell=True)
         s = p.communicate()[0]
         t = tasksJSON.search(s)
@@ -58,9 +35,7 @@ class GruntRunner(object):
         self.folders = []
         for f in self.window.folders():
             self.folders.append(f)
-            if os.path.exists(os.path.join(f, "grunt.js")):
-                self.grunt_files.append(os.path.join(f, "grunt.js"))
-            elif os.path.exists(os.path.join(f, "Gruntfile.js")):
+            if os.path.exists(os.path.join(f, "Gruntfile.js")):
                 self.grunt_files.append(os.path.join(f, "Gruntfile.js"))
         if len(self.grunt_files) > 0:
             if len(self.grunt_files) == 1:
@@ -68,7 +43,7 @@ class GruntRunner(object):
             else:
                 self.window.show_quick_panel(self.grunt_files, self.choose_file)
         else:
-            sublime.error_message("Gruntfile not found!")
+            sublime.error_message("Gruntfile.js not found!")
 
     def choose_file(self, file):
         self.wd = os.path.dirname(self.grunt_files[file])
@@ -84,7 +59,12 @@ class GruntRunner(object):
 def settings():
     return sublime.load_settings('SublimeGrunt.sublime-settings')
 
-class GruntCommand(sublime_plugin.TextCommand):
-    def run(self, edit):
-        self.window = self.view.window()
+class GruntCommand(sublime_plugin.WindowCommand):
+    def run(self):
+        #self.window = self.view.window()
         GruntRunner(self.window)
+
+class GruntKillCommand(sublime_plugin.WindowCommand):
+    def run(self):
+        #self.window = self.view.window()
+        self.window.run_command("exec", {"kill": True})
