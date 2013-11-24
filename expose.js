c0d3e88ee2716e9a897d0d1af6a01f7acfd203c1
Benny Thomas <jan.thomas@speilbildet.com>
2013-11-24 14:26:45 +0100

    Added support for running targets of task

--- expose.js
+++ expose.js
@@ -7,13 +7,28 @@ module.exports = function(grunt) {
     'expose', "Expose available tasks as JSON object.", function () {
       var tasks = grunt.task._tasks;
       _.each( tasks, function( value, key, list ) {
-        var targets = Object.keys(grunt.config.getRaw( key ) || {});
-        if ( targets.length > 0 ) {
+        // We don't want to shouw or own task
+        if (key === 'expose') {
+          delete list[key];
+        }
+        else {
+          // Filter away reservered words that are none-targets
+          var targets = _.difference(Object.keys(grunt.config.getRaw( key ) || {}), ['files', 'options', 'globals']);
+          
+          if ( targets.length > 0 ) {
             list[ key ].targets = targets;
+          
+            if ( targets.length > 1 ) {
+              _.each( targets, function(target) {
+                var name = key + ":" + target;
+
+                list[name] = { name: name, info: 'Targets ' + name + '. ' + list[key].info || '', meta: { info: list[key].meta&&list[key].meta.info} };
+              });
+            }
+          }
         }
       });
       grunt.log.write("EXPOSE_BEGIN" + JSON.stringify(tasks) + "EXPOSE_END");
     }
   );
-
 };
